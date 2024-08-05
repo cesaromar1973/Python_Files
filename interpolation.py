d9810538674e1c6501a2e95a18215ba5147a270e
cesaromar1973 <cesaromar1973@gmai.com>
2024-08-05 17:02:27 -0400

    Courses on Python programming from online UDEMY courses

---
+++ interpolation.py
@@ -0,0 +1,7 @@
+age = 24
+print("My age is %d years" % age)
+
+major = "Years"
+minor = "Months"
+print("My age is %d %s, %d %s" % (age, major, 6, minor))
+print("Pi is approximately %f" % (22 / 7))
---
+++ strings2.py
@@ -0,0 +1,11 @@
+#                   1
+#         012345678901234
+parrot = "Norwegian Blue"
+
+print(parrot[0:6])      #Norweg
+print(parrot[3:5])      #we
+print(parrot[10:14])    #Blue
+
+number = "9,223;372:036,854,775;807"
+print(number [1::4])
+ 
