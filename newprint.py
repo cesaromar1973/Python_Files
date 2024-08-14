c0a34067e16a4af9906ff45f72801e68691b4d7d
cesaromar1973 <cesaromar1973@gmai.com>
2024-08-13 20:56:37 -0400

    Merge remote-tracking branch 'origin/master'

---
+++ newprint.py
@@ -0,0 +1,5 @@
+name = "Ceasr"
+age = 45
+
+print(name, age, "Python", 2024)
+print(name, age, "Python", 2024, sep=", ")
---
+++ splitting.py
@@ -0,0 +1,5 @@
+panagram = """The quick brown fox 
+jumps\tover the lazy dog"""
+
+words = panagram.split()
+print(words)
